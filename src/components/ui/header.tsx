61706fe897804e9c59a3e3b00920aa5c40a4fe60
HowBaka <gravy.thom@gmail.com>
2024-06-03 09:29:18 +0200

    [INIT] Base pomodoro

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="flex items-center justify-center px-4 py-3 bg-zinc-900 dark:bg-zinc-950">
+            <h1 className="text-lg font-semibold text-zinc-50 dark:text-zinc-50">Pomodoro</h1>
+        </header>
+    )
+}
