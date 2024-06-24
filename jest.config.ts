7a699e9d37d9fde96c0bbb1e30b5b5db1879aefb
Teyir <60007549+Teyir@users.noreply.github.com>
2024-06-24 14:01:24 +0200

    [IMPROVE] coverage config

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,27 @@
-import type { Config } from 'jest'
-import nextJest from 'next/jest.js'
+import type {Config} from 'jest';
+import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
-  dir: './',
-})
+    dir: './',
+});
 
 const config: Config = {
-  coverageProvider: 'v8',
-  testEnvironment: 'jsdom',
-}
+    coverageProvider: 'v8',
+    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts,tsx}',
+        '!src/**/*.d.ts',
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
+    reporters: [
+        'default',
+        ['jest-junit', {outputDirectory: 'coverage', outputName: 'test-report.xml'}],
+    ],
+};
 
-export default createJestConfig(config)
+export default createJestConfig(config);
